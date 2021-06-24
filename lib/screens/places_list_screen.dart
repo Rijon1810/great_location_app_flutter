d5b7c7871fc0b5f360e4da8ca6bc8c6bf0549ce8
Rokibul Hasan Rijon <44047172+Rijon1810@users.noreply.github.com>
2021-06-24 19:10:21 +0600

    5 number video have to see

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:great_location_app_flutter/screens/add_place_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
